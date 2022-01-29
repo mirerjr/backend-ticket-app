e65707c3b407ec04c29f3b3d7b9f4512fa623789
Mirer Junior <mirer.rmj@gmail.com>
2022-01-29 20:17:03 -0300

    Refactoring database functions to a class

--- src/db/database.js
+++ src/db/database.js
@@ -1,51 +1,59 @@
-const connection = require("./conection");
 
-async function select(entity){
-    const database = await connection.connect();
-    const response = await database.query(`SELECT * FROM ${entity}`);
-    
-    return response.rows;
-}
+module.exports = class Database {
+    #connection = null;
+    entity = null;
 
-async function insert(entity, fields, values){
-    const database = await connection.connect();
-    const valueParams = getValueParamsList(values);
-    const fieldParams = fields.join(',');
-    const sql = `INSERT INTO ${entity}(${fieldParams}) VALUES (${valueParams});`;
-
-    return await database.query(sql, values);
-}
-
-async function update(entity, fields, values, id){
-    const database = await connection.connect();
-    const idParam = `$${fields.length + 1}`;
-    const fieldParams = getFieldParamsList(fields);
-    const sql = `UPDATE ${entity} SET ${fieldParams} WHERE id=${idParam}`;
-
-    return await database.query(sql, [...values, id]);
-}
-
-
-//Returns a list with params ($1, $2...) based on value lenght
-function getValueParamsList(values){
-    let params = [];
-
-    for(let count = 0; count < values.length; count++){
-        params.push(`$${count+1}`);
-    }
- 
-    return params.join(',');
-}
-
-//Return a map with name of field as a key, and params ($1, $2...) as a value
-function getFieldParamsList(fields){
-    let params = [];
-
-    for(let count = 0; count < fields.length; count++){
-        params.push(`${fields[count]}=$${count+1}`);
+    constructor(entity){
+        this.#connection = require("./conection");
+        this.entity = entity
     }
 
-    return params.join(',');
-}
+    async select(){
+        const database = await this.#connection.connect();
+        const response = await database.query(`SELECT * FROM ${this.entity} ORDER BY id DESC`);
 
-module.exports = { select, insert, update }
+        return response.rows;
+    }
+
+    async insert(fields, values){
+        const database = await this.#connection.connect();
+        const valueParams = this.#getValueParamsList(values);
+        const fieldParams = fields.join(',');
+        
+        const sql = `INSERT INTO ${this.entity}(${fieldParams}) VALUES (${valueParams});`;
+
+        return await database.query(sql, values);        
+    }
+
+    async update(id, fields, values){
+        const database = await this.#connection.connect();
+        const idParam = `$${fields.length + 1}`;
+        const fieldParams = this.#getFieldParamsList(fields);
+
+        const sql = `UPDATE ${this.entity} SET ${fieldParams} WHERE id=${idParam}`;
+
+        return await database.query(sql, [...values, id]);
+    }
+
+    //Returns a list with params ($1, $2...) based on value lenght
+    #getValueParamsList(values){
+        let params = [];
+
+        for(let count = 0; count < values.length; count++){
+            params.push(`$${count+1}`);
+        }
+
+        return params.join(',');
+    }
+
+    //Return a map with name of field as a key, and params ($1, $2...) as a value
+    #getFieldParamsList(fields){
+        let params = [];
+
+        for(let count = 0; count < fields.length; count++){
+            params.push(`${fields[count]}=$${count+1}`);
+        }
+
+        return params.join(',');
+    }
+}
