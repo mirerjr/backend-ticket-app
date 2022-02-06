4d0858e00b6a98138b3b6abeffd69272186d9115
Mirer Junior <mirer.rmj@gmail.com>
2022-02-06 17:48:25 -0300

    Added CRUD for ticket-status

--- src/model/ticket-status.js
+++ src/model/ticket-status.js
@@ -1,9 +1,56 @@
+const Database = require("../db/database")
+
 class TicketStatus{
     id = null
     statusName = null
     
-    constructor(statusName){
-        this.statusName = statusName
+    constructor(data){
+        Object.assign(this, data)
+    }
+
+    static async get(id){
+        const database = new Database('ticket_status')
+        const data = await database.selectById(id)
+
+        if(data){
+            return new this(data[0])
+        }
+
+        return null
+    }
+
+    static async findAll(){
+        const database = new Database('ticket_status')
+        const data = await database.select()
+
+        if(data){
+            const result = []
+
+            data.forEach((row, index) => {
+                result[index] = new this(row)
+            })
+
+            return result
+        }
+
+        return null
+    }
+
+    async save(){
+        const database = new Database('ticket_status')
+
+        if(this.id){
+            const result = await database.selectById(this.id)
+
+            if(result){
+                const fields = Object.keys(this).slice(1)
+                const values = Object.values(this).slice(1)
+
+                return await database.update(this.id, fields, values)
+            }
+        }
+
+        return await database.insert(['status_name'], [this.statusName])
     }
 
 }
