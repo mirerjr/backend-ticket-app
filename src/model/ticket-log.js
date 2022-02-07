32f2a9b503e00b23da2590eb97c17c5a4e26c2de
Mirer Junior <mirer.rmj@gmail.com>
2022-02-06 23:10:12 -0300

    Added CRUD for ticket-log

--- src/model/ticket-log.js
+++ src/model/ticket-log.js
@@ -1,3 +1,5 @@
+const Database = require("../db/database")
+
 class TicketLog{
     id = null
     ticketStatusId = null
@@ -8,14 +10,50 @@ class TicketLog{
     escalated = null
     description = null
 
-    constructor(ticket_status, ticket, status_changed, commented, escalated, description){
-        this.ticket_status_id = ticket_status
-        this.ticket_id = ticket
-        this.date = new Date()
-        this.status_changed = status_changed
-        this.commented = commented
-        this.escalated = escalated
-        this.description = description
+    constructor(data){
+        Object.assign(this, data)
+    }
+
+    static async get(id){
+        const database = new Database('ticket_log')
+        const data = await database.selectById(id)
+
+        if(data){
+            return new this(data[0])
+        }
+
+        return null
+    }
+
+    static async findAll(){
+        const database = new Database('ticket_log')
+        const data = await database.select()
+        const result = []
+
+        if(data){
+            data.forEach((row, index) => {
+                result[index] = new this(row)
+            })
+        }
+
+        return result
+    }
+
+    async save(){
+        const database = new Database('ticket_log')
+
+        const fields = Object.keys(this).slice(1)
+        const values = Object.values(this).slice(1)
+
+        if(this.id){
+            const result = await database.selectById(this.id)
+
+            if(result){
+                return await database.update(this.id, fields, values)
+            }
+        }
+
+        return await database.insert(fields, values)
     }
 
 }
