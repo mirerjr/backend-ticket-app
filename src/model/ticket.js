701e5aac0cbeeea6374f62e45c99cf36fb9663ed
Mirer Junior <mirer.rmj@gmail.com>
2022-02-06 23:01:04 -0300

    Added CRUD for ticket

--- src/model/ticket.js
+++ src/model/ticket.js
@@ -1,3 +1,5 @@
+const Database = require("../db/database")
+
 class Ticket{
     id = null
     personId = null
@@ -8,14 +10,50 @@ class Ticket{
     open = null
     important = null
 
-    constructor(person, ticket_type, description, title, important){
-        this.person_id = person
-        this.ticket_type_id = ticket_type
-        this.date_created = new Date()
-        this.description = description
-        this.title = title
-        this.open = true
-        this.important = important
+    constructor(data){
+        Object.assign(this, data)
+    }
+
+    static async get(id){
+        const database = new Database('ticket')
+        const data = await database.selectById(id)
+
+        if(data){
+            return new this(data[0])
+        }
+
+        return null
+    }
+
+    static async findAll(){
+        const database = new Database('ticket')
+        const data = await database.select()
+        const result = []
+
+        if(data){
+            data.forEach((row, index) => {
+                result[index] = new this(row)
+            })
+        }
+
+        return result
+    }
+
+    async save(){
+        const database = new Database('ticket')
+
+        const fields = Object.keys(this).slice(1)
+        const values = Object.values(this).slice(1)
+
+        if(this.id){
+            const result = await database.selectById(this.id)
+
+            if(result){
+                return await database.update(this.id, fields, values)
+            }
+        }
+
+        return await database.insert(fields, values)
     }
 
 
